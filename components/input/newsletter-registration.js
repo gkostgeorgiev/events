69552301bf9eb3ed372883c94ebc8adfe09f8454
Georgi Ko. Georgiev (A1 BG) <Georgi.Ko.Georgiev@a1.bg>
2023-08-02 10:14:04 +0300

    set up api endpoints for leaving comments

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,10 +1,25 @@
-import classes from './newsletter-registration.module.css';
+import { useRef } from "react";
+import classes from "./newsletter-registration.module.css";
 
 function NewsletterRegistration() {
+  const emaiLInputRef = useRef();
+
   function registrationHandler(event) {
     event.preventDefault();
 
+    const enteredEmail = emaiLInputRef.current.value;
+
     // fetch user input (state or refs)
+    fetch("/api/newsletter", {
+      method: "POST",
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
     // optional: validate input
     // send valid data to API
   }
@@ -15,10 +30,11 @@ function NewsletterRegistration() {
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
+            ref={emaiLInputRef}
           />
           <button>Register</button>
         </div>
