4a28654f8ee73a0aba8a9ea99d1afe958500a1a5
Georgi Ko. Georgiev (A1 BG) <Georgi.Ko.Georgiev@a1.bg>
2023-08-03 10:24:08 +0300

    connected MongoDB to run MongoDB queries from Inside the API routes.

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,11 +1,33 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function handler(req, res) {
   if (req.method === "POST") {
-    const userEmail = req.body.email;
-    if (!userEmail || !userEmail.includes("@")) {
-      return res.status(422).json({ message: "Invalid email address" });
+    try {
+      const userEmail = req.body.email;
+      if (!userEmail || !userEmail.includes("@")) {
+        return res.status(422).json({ message: "Invalid email address" });
+      }
+
+      const client = await MongoClient.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      const db = client.db();
+
+      await db.collection("emails").insertOne({ email: userEmail });
+
+      client.close();
+
+      return res.status(201).json({ message: "Signed up" });
+    } catch (err) {
+      console.error(err);
+      console.log(err.message);
+      return res.status(400).json({ message: "Bad request" });
     }
-    console.log(userEmail);
-    res.status(201).json({ message: "Signed up" });
   }
 }
 
